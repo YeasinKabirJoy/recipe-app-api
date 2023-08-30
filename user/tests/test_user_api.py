9910801afae6b3140223c33a3783c46a8cd90eeb
Yeasin Kabir Joy <52117661+YeasinKabirJoy@users.noreply.github.com>
2023-08-30 19:21:09 +0600

    start

---
+++ user/tests/test_user_api.py
@@ -0,0 +1,10 @@
+# from django.test import SimpleTestCase
+#
+#
+# class CalcTests(SimpleTestCase):
+#     """Test the calc module."""
+#
+#     def test_add_numbers(self):
+#         self.assertEqual(13, 13)
+
+
